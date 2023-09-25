8e83b94b444160c32bc7afba22743a79e59f4bf3
joaberg <58996735+joaberg@users.noreply.github.com>
2023-09-25 13:37:08 +0200

    Create flake.nix

---
+++ .config/home-manager/flake.nix
@@ -0,0 +1,52 @@
+{
+  description = "Home Manager configuration of joakim";
+
+  inputs = {
+    # Specify the source of Home Manager and Nixpkgs.
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    hyprland.url = "github:hyprwm/Hyprland";
+   
+  };
+
+  outputs = { self, nixpkgs, home-manager, hyprland, ... }:
+    let
+      system = "x86_64-linux";
+  	  pkgs = import nixpkgs {
+        inherit system;
+        config = {
+          allowUnfree = true;
+        };
+      };
+      #pkgs = nixpkgs.legacyPackages.${system};
+      
+      	
+    in {
+      homeConfigurations."joakim" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+
+        # Specify your home configuration modules here, for example,
+        # the path to your home.nix.
+        modules = [ 
+        	./home.nix 
+			    hyprland.homeManagerModules.default
+			    {wayland.windowManager.hyprland.enable = true;}
+                   
+
+        ];
+
+        # Optionally use extraSpecialArgs
+        # to pass through arguments to home.nix
+        
+      };
+
+
+
+
+      
+      
+    };
+}
